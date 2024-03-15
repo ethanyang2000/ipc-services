5b38ef21ef68124d2d5d7048d8a208fb04f83577
Yuxiang Yang <45881384+ethanyang2000@users.noreply.github.com>
2024-03-15 01:40:45 +0000

    udpate exp

---
+++ exp.py
@@ -0,0 +1,72 @@
+# import subprocess
+# import os
+
+# # Define the combinations of segment sizes and numbers of segments
+# segment_sizes = [32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
+# num_segments = [1, 3, 5]
+
+# # Command template for the service
+# service_cmd_template = './bin/service --n_sms {n_sms} --sms_size {sms_size}'
+
+# # Command for the app
+# app_cmd = './bin/app --state ASYNC --file bin/input/Small.jpg'
+
+# # Set the environment variable for LD_LIBRARY_PATH
+# os.environ['LD_LIBRARY_PATH'] = 'snappy-c:' + os.environ.get('LD_LIBRARY_PATH', '')
+
+# for sms_size in segment_sizes:
+#     for n_sms in num_segments:
+#         # Format the service command with the current sms_size and n_sms
+#         service_cmd = service_cmd_template.format(n_sms=n_sms, sms_size=sms_size)
+#         print(f"Executing: {service_cmd}")
+        
+#         # Execute the service command
+#         subprocess.run(service_cmd, shell=True, check=True)
+
+#         # Execute the app command
+#         print("Executing the app command...")
+#         subprocess.run(app_cmd, shell=True, check=True)
+
+#         print("Completed cycle for sms_size={} and n_sms={}\n".format(sms_size, n_sms))
+import subprocess
+import os
+import signal
+
+# Define the combinations of segment sizes and number of segments
+segment_sizes = [32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
+num_segments = [1, 3, 5]
+
+# Fixed parameters for the second command
+state = "ASYNC"
+file = "bin/input/Huge.jpg"
+
+# Iterate over the combinations of segment sizes and number of segments
+for sms_size in segment_sizes:
+    for n_sms in num_segments:
+        # Set the LD_LIBRARY_PATH environment variable
+        env = os.environ.copy()
+        env['LD_LIBRARY_PATH'] = 'snappy-c:' + env.get('LD_LIBRARY_PATH', '')
+        
+        # Start the service daemon
+        service_cmd = f"./bin/service --n_sms {n_sms} --sms_size {sms_size}"
+        print(f"Starting service with sms_size={sms_size} and n_sms={n_sms}")
+        service_process = subprocess.Popen(service_cmd, shell=True, env=env)
+        
+        try:
+            # Execute the second command
+            second_cmd = f"./bin/app --state {state} --file {file}"
+            print(f"Executing: {second_cmd}")
+            subprocess.run(second_cmd, shell=True, check=True)
+            
+        finally:
+            # Ensure the service daemon is killed
+            service_process.send_signal(signal.SIGINT) # Attempt graceful shutdown
+            # service_process.wait(tßimeout=3) # Wait up to 5 seconds for the process to exit
+            if service_process.poll() is None: # If it's still running, force kill
+                print("Service process did not exit, forcefully killing.")
+                service_process.kill()
+                service_process.wait() # Ensure the process has been killed
+        
+        print(f"Cycle with sms_size={sms_size} and n_sms={n_sms} completed.\n")
+
+print("All cycles completed.")
--- src/app.cpp
+++ src/app.cpp
@@ -1,4 +1,6 @@
 #include "tinylib.hpp"
+#include <stdio.h>
+#include <sys/time.h>
 static tiny_client* c;
 static bool done = false;
 
@@ -10,6 +12,11 @@ void sig_exit_handler(int signal) {
 void callback_func(char* inp){
     std::cout<<"callback called after done"<<std::endl;
     done = true;
+}
+long calculateCST(struct timeval start, struct timeval end) {
+    long seconds = (end.tv_sec - start.tv_sec);
+    long micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
+    return micros;
 }
 
 int main(int argc, char *argv[]){
@@ -32,10 +39,33 @@ int main(int argc, char *argv[]){
             files = argv[++i];
         }
     }
-    bool state_bool = state == "SYNC"? false:true;
+    bool state_bool = true;
+    
+    struct timeval startTime, returntime, endTime;
+    gettimeofday(&startTime, NULL);
+
     c = new tiny_client();
     c->Compress(file, state_bool, callback_func);
+    gettimeofday(&returntime, NULL);
     std::cout<<"nonblocking after async call"<<std::endl;
     while(!done){}
+    gettimeofday(&endTime, NULL);
+    long returntime1 = calculateCST(startTime, returntime);
+    long cst = calculateCST(startTime, endTime);
+    printf("Asyncronized Client-Side main Thread return Time (CST): %ld microseconds\n", returntime1);
+    printf("Asyncronized Client-Side Callback Return Time (CST): %ld microseconds\n", cst);
+    //delete c;
+
+    //sync call
+    state_bool = false;
+    gettimeofday(&startTime, NULL);
+
+    //c = new tiny_client();
+    c->Compress(file, state_bool, nullptr);
+    
+    std::cout<<"nonblocking after async call"<<std::endl;
+    gettimeofday(&endTime, NULL);
+    cst = calculateCST(startTime, endTime);
+    printf("Syncronized Client-Side Service Time (CST): %ld microseconds\n", cst);
     delete c;
 }
